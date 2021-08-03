29403ae19a343d8137c136d263d7208c80fbb94d
Catherine <rubypromise@gmx.com>
2021-08-03 20:32:03 +0100

    introduce guard statement for empty input

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,5 @@
 let isPalindrome = (phrase) => {
+   if (phrase === '') throw new Error('empty strings are not palindromes');
    if (phrase.trim() === '') return false;
    return phrase === phrase.split('').reverse().join('')
 };
@@ -17,11 +18,15 @@ describe('the palindrome canary spec', () => {
   it('false for dad mom', () => {
     expect (isPalindrome('dude')).toBe(false);
   });
-    it('true for whitespace',() => {
+    it('false for whitespace',() => {
     expect(isPalindrome('    ')).toBe(false);
   });
 
-  it.todo('error for empty string');
+  it('error for empty string', () => {
+  expect(() => {
+  isPalindrome('');
+  }).toThrowError('empty strings are not palindromes');
+  });
 
   it.todo('error for not a string');
 
