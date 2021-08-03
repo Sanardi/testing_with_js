64aeb3b528db9fc2dbf8cb5d7e810308274a65a5
Catherine <rubypromise@gmx.com>
2021-08-03 20:25:35 +0100

    introduce guard clause for emtpty string

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,7 @@
-let isPalindrome = (phrase) => phrase === phrase.split('').reverse().join('');
+let isPalindrome = (phrase) => {
+   if (phrase.trim() === '') return false;
+   return phrase === phrase.split('').reverse().join('')
+};
 
 describe('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
@@ -14,9 +17,9 @@ describe('the palindrome canary spec', () => {
   it('false for dad mom', () => {
     expect (isPalindrome('dude')).toBe(false);
   });
-
-
-  it.todo('true for whitespace');
+    it('true for whitespace',() => {
+    expect(isPalindrome('    ')).toBe(false);
+  });
 
   it.todo('error for empty string');
 
